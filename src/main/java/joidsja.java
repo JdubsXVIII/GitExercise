5daf8c4c53a277672bca4d64df2f163d5872d0b9
JdubsXVIII <126510635+JdubsXVIII@users.noreply.github.com>
2025-02-17 15:15:15 -0500

    Added the power of the Elf King of the Dwarves to joidsja

--- src/main/java/joidsja.java
+++ src/main/java/joidsja.java
@@ -2,5 +2,6 @@ public class joidsja {
 
     public static void main(String[] args){
         System.out.println("hello world!");
+        System.out.println("Cacame Khuzdul, Elf King of the Dwarves, bashes your face in.");
     }
 }
