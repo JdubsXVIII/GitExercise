3e1f6beabf782936e50b4e17d682f8631c773ec0
JdubsXVIII <126510635+JdubsXVIII@users.noreply.github.com>
2025-02-17 15:24:48 -0500

    Added the fiery power of the Burninator to the joidsja method.

--- src/main/java/joidsja.java
+++ src/main/java/joidsja.java
@@ -3,5 +3,6 @@ public class joidsja {
     public static void main(String[] args){
         System.out.println("hello world!");
         System.out.println("Cacame Khuzdul, Elf King of the Dwarves, bashes your face in.");
+        System.out.println("Trogdor, the Burninator, burninates all the thatched-roof cottages.");
     }
 }
